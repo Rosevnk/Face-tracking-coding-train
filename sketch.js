a951943f4f640cb28b8e7a7fc79feb73ac953ebb
Rosevnk <rosevanek1@gmail.com>
2024-12-23 14:16:57 +0100

    Face boxes

--- sketch.js
+++ sketch.js
@@ -32,11 +32,38 @@ function draw() {
 
   for (let i = 0; i < faces.length; i++) {
     let face = faces[i];
+
+    // Draw every 5th keypoint
     for (let j = 0; j < face.keypoints.length; j++) {
-      let keypoint = face.keypoints[j];
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 5);
+      if (j % 5 === 0) {
+        let keypoint = face.keypoints[j];
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 5);
+      }
     }
+
+    // Define keypoint indices for features
+    let leftEye = [46, 55, 117, 174]; // Indices for left eye
+    let rightEye = [285, 300, 399, 346]; // Indices for right eye
+
+    // Draw boxes around the eyes
+    drawFeatureBox(face, leftEye, [255, 0, 0]); // Red box for left eye
+    drawFeatureBox(face, rightEye, [0, 0, 255]); // Blue box for right eye
   }
+}
+
+// Function to draw a bounding box around a set of keypoints
+function drawFeatureBox(face, indices, color) {
+  let xMin = width, xMax = 0, yMin = height, yMax = 0;
+  for (let i of indices) {
+    let keypoint = face.keypoints[i];
+    xMin = min(xMin, keypoint.x);
+    xMax = max(xMax, keypoint.x);
+    yMin = min(yMin, keypoint.y);
+    yMax = max(yMax, keypoint.y);
+  }
+  noFill();
+  stroke(color[0], color[1], color[2]);
+  rect(xMin, yMin, xMax - xMin, yMax - yMin);
 }
