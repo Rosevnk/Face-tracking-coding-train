f43cb209c1c797491dcbb75944ba3a29aba592e7
Rosevnk <rosevanek1@gmail.com>
2024-12-23 13:06:01 +0100

    preload
    preload function load a model configed as options specifies and store it in the faceMesh variable.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 let faceMesh;
 let options = {maxFaces: 1, refineLandmarks: false, flipped: false};
 
+function preload() {
+  faceMesh= ml5.faceMesh(options);
+}
 
 function setup() {
   createCanvas(400, 400);
