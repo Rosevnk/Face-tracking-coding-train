04bead49562279b37555322c6e623daa324a6aa4
Rosevnk <rosevanek1@gmail.com>
2024-12-23 13:10:06 +0100

    Fetch webcam video
    Define a variable video to hold the webcam video
    
    Resize the canvas dimensions to 640x480, a common resolution for webcams
    
    Fetch the webcam video, resize it to fit the canvas, and hide it from the display

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
 let faceMesh;
 let options = {maxFaces: 1, refineLandmarks: false, flipped: false};
+let video;
 
 function preload() {
   faceMesh= ml5.faceMesh(options);
 }
 
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(640, 480);
+
+    // Create the video and hide it
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.hide();
+  }
+  
 
 function draw() {
   background(220);
